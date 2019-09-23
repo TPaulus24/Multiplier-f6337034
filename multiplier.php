2bd522ce03f2fc4dcf7a68d2618b13490a40dea7
FIRST_TOM LAST_PAULUS <54597104+TPaulus24@users.noreply.github.com>
2019-09-23 09:30:23 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,7 @@
+<?php
+echo "Ik ga twee getallen met elkaar vermenigvuldigen" . PHP_EOL;
+echo "Eerste getal?" . PHP_EOL;
+$num1 = readLine("> ");
+echo "Tweede getal?" . PHP_EOL;
+$num2 = readLine("> ");
+echo ("Uw resultaat is: ". $num1 * $num2);
